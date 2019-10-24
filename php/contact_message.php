2c39979d9de334daf064a07df98cd5d70df267cd
Hansel <hansel.donizete@gmail.com>
2019-10-24 11:08:54 -0300

    validação do formulário no backend

--- php/contact_message.php
+++ php/contact_message.php
@@ -4,29 +4,13 @@
 //echo var_dump($_POST);
 
 
+$arq_json = (isset($_POST)) ? json_encode($_POST) : null;
 
-$arq_json = json_encode($_POST);
-
-
-//echo $arq_json;
-
-
-
-/*
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$assunto = $_POST['assunto'];
-$descricao = $_POST['descricao'];
-
-echo $nome;
-*/
-
-
-
-$name = 'contact_message.txt';
-$file = fopen($name, 'a');
-fwrite($file, $arq_json);
-
-fclose($file);
-
+if(!empty($arq_json))
+{
+    $name = 'contact_message.txt';
+    $file = fopen($name, 'a');
+    fwrite($file, $arq_json);
+    fclose($file);
+}
 ?>
